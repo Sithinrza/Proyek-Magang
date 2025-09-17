e7994eba1c866a6ae9a2024dd137e32c54a611ef
Sithinrza <sithinrza@gmail.com>
2025-09-17 16:24:04 +0800

    menambahkan tombol

--- resources/views/admin/team/index.blade.php
+++ resources/views/admin/team/index.blade.php
@@ -2,59 +2,60 @@
     <x-slot:title>
         Manajemen Tim
     </x-slot:title>
+    <section class="p-10 md:p-20">
+        <div class="flex justify-between items-center mb-6">
+            <h1 class="text-2xl font-bold text-gray-800">Manajemen Anggota Tim</h1>
+            <a href="{{ route('admin.team.create') }}" class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                </svg>
+                Tambah Anggota
+            </a>
+        </div>
 
-    <div class="flex justify-between items-center mb-6">
-        <h1 class="text-2xl font-bold text-gray-800">Manajemen Anggota Tim</h1>
-        <a href="{{ route('admin.team.create') }}" class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
-            </svg>
-            Tambah Anggota
-        </a>
-    </div>
-
-    <div class="bg-white shadow-md rounded-lg overflow-x-auto">
-        <table class="table table-zebra w-full">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Foto</th>
-                    <th>Nama</th>
-                    <th>Posisi</th>
-                    <th class="text-right">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($teams as $team)
-                    <tr class="hover">
-                        <td>{{ $team->id }}</td>
-                        <td>
-                            <div class="avatar">
-                                <div class="w-12 h-12 rounded-full">
-                                    <img src="{{ $team->getImage() ?: 'https://via.placeholder.com/150' }}" alt="{{ $team->name }}" />
-                                </div>
-                            </div>
-                        </td>
-                        <td>{{ $team->name }}</td>
-                        <td>{{ $team->position }}</td>
-                        <td class="text-right">
-                            <div class="flex justify-end space-x-2">
-                                <a href="{{ route('admin.team.edit', $team->id) }}" class="btn btn-sm btn-outline btn-warning">Edit</a>
-                                <form action="{{ route('admin.team.destroy', $team->id) }}" method="POST" onsubmit="return confirm('Anda yakin ingin menghapus anggota ini?');">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-outline btn-error">Hapus</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @empty
+        <div class="bg-white shadow-md rounded-lg overflow-x-auto">
+            <table class="table table-zebra w-full">
+                <thead>
                     <tr>
-                        <td colspan="5" class="text-center p-4">Belum ada data anggota tim.</td>
+                        <th>ID</th>
+                        <th>Foto</th>
+                        <th>Nama</th>
+                        <th>Posisi</th>
+                        <th class="text-right">Aksi</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                    @forelse ($teams as $team)
+                        <tr class="hover">
+                            <td>{{ $team->id }}</td>
+                            <td>
+                                <div class="avatar">
+                                    <div class="w-12 h-12 rounded-full">
+                                        <img src="{{ $team->getImage() ?: 'https://via.placeholder.com/150' }}" alt="{{ $team->name }}" />
+                                    </div>
+                                </div>
+                            </td>
+                            <td>{{ $team->name }}</td>
+                            <td>{{ $team->position }}</td>
+                            <td class="text-right">
+                                <div class="flex justify-end space-x-2">
+                                    <a href="{{ route('admin.team.edit', $team->id) }}" class="btn btn-sm btn-outline btn-warning">Edit</a>
+                                    <form action="{{ route('admin.team.destroy', $team->id) }}" method="POST" onsubmit="return confirm('Anda yakin ingin menghapus anggota ini?');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-outline btn-error">Hapus</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center p-4">Belum ada data anggota tim.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </section>
 
 </x-admin-layout>
