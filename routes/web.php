c513f51b7a1363418c6faf7ca72ba0e0571a145c
lisyah123 <kholisyah0519@gmail.com>
2025-09-17 13:59:15 +0800

    admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 
@@ -19,3 +20,12 @@ Route::get('/service2', [PageController::class, 'service2'])->name('service2');
 Route::get('/service3', [PageController::class, 'service3'])->name('service3');
 
 Route::post('/contact', [ContactFormController::class, 'store'])->name('contact.store');
+
+// Route untuk menampilkan halaman login (GET)
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+
+// Route untuk memproses data login yang dikirim dari form (POST)
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+
+// (Opsional tapi direkomendasikan) Route untuk logout
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
